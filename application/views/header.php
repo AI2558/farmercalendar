d626bb5694ad50303cba64c63218cd29f18d49b1
nookskill <nookskill@hotmail.com>
2015-10-03 13:09:48 +0700

    update pages

---
+++ application/views/header.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>ยินดีต้อนรับสู่ปฏิทินล้นเกวียน</title>
+
+	<!-- Bootstrap core CSS -->
+    <?= css_asset('lavish-bootstrap.css'); ?>
+
+    <!-- Custom styles for this template -->
+    <?= css_asset("navbar-fixed-top.css"); ?> 
+
+</head>
+
+<body>
+	    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">ปฏิทินล้นเกวียน</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">หน้าแรก</a></li>
+            <li><a href="#about">เกี่ยวกับ</a></li>
+            <li><a href="#contact">ติดต่อ</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
