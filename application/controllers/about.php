768c7c0224fe820d18a699b7f513f14216de2447
Waranyu Rerkdee <waranyu.1993@gmail.com>
2015-10-14 17:35:59 +0700

    add information

--- application/controllers/about.php
+++ application/controllers/about.php
@@ -1,7 +1,6 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class About extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -18,10 +17,16 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
+	public function __construct() 
+	{
+		parent::__construct();
+		$this -> session -> set_userdata('page', 'about');
+	} 
+	 
 	public function index()
 	{
 		$this->load->view('header');
-		$this->load->view('welcome_message');
+		$this->load->view('about_view');
 		$this->load->view('footer');
 	}
 }
--- application/views/header.php
+++ application/views/header.php
@@ -1,38 +1,53 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>ยินดีต้อนรับสู่ปฏิทินล้นเกวียน</title>
+?>
+<!DOCTYPE html><html lang="en">
+	<head>
 
-	<!-- Bootstrap core CSS -->
-    <?= css_asset('lavish-bootstrap.css'); ?>
+		<meta charset="utf-8">
+		<title>ยินดีต้อนรับสู่ปฏิทินล้นเกวียน</title>
 
-    <!-- Custom styles for this template -->
-    <?= css_asset("navbar-fixed-top.css"); ?> 
+		<!-- Bootstrap core CSS -->
+		<?= css_asset('lavish-bootstrap.css'); ?>
 
-</head>
+		<!-- Custom styles for this template -->
+		<?= css_asset("navbar-fixed-top.css"); ?>
 
-<body>
-	    <!-- Fixed navbar -->
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">ปฏิทินล้นเกวียน</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">หน้าแรก</a></li>
-            <li><a href="#about">เกี่ยวกับ</a></li>
-            <li><a href="#contact">ติดต่อ</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
+		<!-- JQuery -->
+		<?= js_asset("bower_components/jquery/dist/jquery.js"); ?>
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+		<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+		<script>
+			$(function() {
+				$("#datepicker").datepicker();
+			});
+		</script>
+	</head>
+
+	<body>
+		<!-- Fixed navbar -->
+		<?php $page = $this->session->userdata('page'); ?>
+		<nav class="navbar navbar-default navbar-fixed-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">ปฏิทินล้นเกวียน</a>
+				</div>
+				<div id="navbar" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav">
+						<li class="<?= (($page == 'home') ?  'active' : 'last'); ?>">
+							<a href="home">หน้าแรก</a>
+						</li>
+						<li class="<?= (($page == 'about') ?  'active' : 'last'); ?>">
+							<a href="about">เกี่ยวกับ</a>
+						</li>
+					</ul>
+				</div><!--/.nav-collapse -->
+			</div>
+		</nav>
+
